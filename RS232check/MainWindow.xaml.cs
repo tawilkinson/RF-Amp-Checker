3ee3aff06d31f34bb7efc6bb47ae6155c10be378
tomw2005 <tomwilkinson2005@gmail.com>
2017-01-30 16:04:59 +0000

    Initial Commit

---
+++ RS232check/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+
+namespace RS232check
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SerialPort ComPort = new SerialPort();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGetSerialPorts_Click(object sender, EventArgs e)
+        {
+            string[] ArrayComPortsNames = null;
+            int index = -1;
+            string ComPortName = null;
+
+            try
+            {
+                ArrayComPortsNames = SerialPort.GetPortNames();
+                do
+                {
+                    index += 1;
+                    cboPorts.Items.Add(ArrayComPortsNames[index]);
+                }
+                while (!((ArrayComPortsNames[index] == ComPortName) ||
+                (index == ArrayComPortsNames.GetUpperBound(0))));
+            }
+            catch
+            {
+                txtbInfo.Text = "No COM devices detected...\n";
+            }
+            if (index == ArrayComPortsNames.GetUpperBound(0))
+            {
+                ComPortName = ArrayComPortsNames[0];
+            }
+            cboPorts.Text = ArrayComPortsNames[0];
+        }
+
+        private void btnPortState_Click(object sender, EventArgs e)
+        {
+            if (btnPortState.Content.ToString() == "Closed")
+            {
+                btnPortState.Content = "Open";
+                ComPort.PortName = Convert.ToString(cboPorts.Text);
+                ComPort.BaudRate = Convert.ToInt32(19200);
+                ComPort.DataBits = Convert.ToInt16(8);
+                ComPort.Handshake = (Handshake)Enum.Parse(typeof(Handshake), "None");
+                ComPort.Open();
+            }
+            else if (btnPortState.Content.ToString() == "Open")
+            {
+                btnPortState.Content = "Closed";
+                ComPort.Close();
+            }
+        }
+    }
+}
