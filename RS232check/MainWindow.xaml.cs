7a990ce63f86ed53c4bda05893173c48f6c52fb2
tomw2005 <tomwilkinson2005@gmail.com>
2017-01-31 15:28:12 +0000

    Working but puts responses to MessageBox. Probably a string issue that needs cleaning up.

--- RS232check/MainWindow.xaml.cs
+++ RS232check/MainWindow.xaml.cs
@@ -25,10 +25,30 @@ namespace RS232check
     public partial class MainWindow : Window
     {
         SerialPort ComPort = new SerialPort();
+        bool PortFlag = new bool();
+        Dictionary<string, string> Commands = new Dictionary<string, string>();
 
         public MainWindow()
         {
             InitializeComponent();
+
+            Commands.Add("Enable Remote Control", "SPC:CTL 1;");
+            Commands.Add("Enable RF power", "RF 1;");
+            Commands.Add("Disable RF power", "RF 0;");
+            Commands.Add("Get status", "STA?;");
+            Commands.Add("Get forward power", "PWR:PF?;");
+            Commands.Add("Get temperature", "SPC:T?;");
+
+            foreach (KeyValuePair<string, string> entry in Commands)
+            {
+                cboCommand.Items.Add(entry.Key);
+            }
+        }
+
+        private void SetMessage(string txtin)
+        {
+            string txtout = txtin + "\n";
+            txtbInfo.Text = txtout;
         }
 
         private void btnGetSerialPorts_Click(object sender, EventArgs e)
@@ -47,34 +67,76 @@ namespace RS232check
                 }
                 while (!((ArrayComPortsNames[index] == ComPortName) ||
                 (index == ArrayComPortsNames.GetUpperBound(0))));
+
+                if (index == ArrayComPortsNames.GetUpperBound(0))
+                {
+                    ComPortName = ArrayComPortsNames[0];
+                }
+                cboPorts.Text = ArrayComPortsNames[0];
+                PortFlag = true;
             }
             catch
             {
-                txtbInfo.Text = "No COM devices detected...\n";
+                SetMessage("No COM devices detected...");
+                PortFlag = false;
             }
-            if (index == ArrayComPortsNames.GetUpperBound(0))
-            {
-                ComPortName = ArrayComPortsNames[0];
-            }
-            cboPorts.Text = ArrayComPortsNames[0];
         }
 
-        private void btnPortState_Click(object sender, EventArgs e)
+        public void btnPortStatus_Click(object sender, EventArgs e)
         {
-            if (btnPortState.Content.ToString() == "Closed")
+            if (PortFlag == false)
+            {
+                SetMessage("First get system ports or choose a port.");
+            }
+            else if (btnPortState.Content.ToString() == "Closed")
             {
                 btnPortState.Content = "Open";
                 ComPort.PortName = Convert.ToString(cboPorts.Text);
                 ComPort.BaudRate = Convert.ToInt32(19200);
                 ComPort.DataBits = Convert.ToInt16(8);
                 ComPort.Handshake = (Handshake)Enum.Parse(typeof(Handshake), "None");
+                ComPort.DataReceived += new SerialDataReceivedEventHandler(DataRecievedHandler);
                 ComPort.Open();
+                SetMessage(cboPorts.Text + " opened.");
             }
             else if (btnPortState.Content.ToString() == "Open")
             {
                 btnPortState.Content = "Closed";
                 ComPort.Close();
+                SetMessage(cboPorts.Text + " closed.");
             }
         }
+
+        private void DataRecievedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            string indata = sp.ReadExisting();
+            // SetMessage("Data Received:\n" + indata);
+            MessageBox.Show("Returned value: \n" + indata);
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            if (cboCommand.SelectedValue == null)
+            {
+                SetMessage("First select a command to send.");
+            }
+            else if (PortFlag == false)
+            {
+                SetMessage("First get system ports.");
+            }
+            else if (btnPortState.Content.ToString() == "Closed")
+            {
+                SetMessage("First choose a port.");
+            }
+            else
+            {
+                string val = "";
+                Commands.TryGetValue(cboCommand.SelectedValue.ToString(), out val);
+                SetMessage("Sending \"" + val + "\"");
+                ComPort.Write(val);
+            }
+        }
+
     }
 }
